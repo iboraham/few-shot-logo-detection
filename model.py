3d3084857e8065bdeda8793a1d017b21ff54db64
Onur Şerbetçi <onurserbetcii@gmail.com>
2022-12-19 23:36:07 +0000

    Fix: Model backbone forwarding two times

--- model.py
+++ model.py
@@ -8,30 +8,21 @@ class LogoDetectionModel(torch.nn.Module):
         super(LogoDetectionModel, self).__init__()
 
         # Use a pretrained backbone such as ResNet or VGG
-        self.backbone = resnet18(weights=ResNet18_Weights.DEFAULT)
+        self.model = resnet18(weights=ResNet18_Weights.DEFAULT)
 
-        # Freeze the weights of the backbone
-        for param in self.backbone.parameters():
+        # Freeze the weights of the backbone so that they are not updated
+        for param in self.model.parameters():
             param.requires_grad = False
 
-        # Drop the last classification layer of the backbone
-
-        # Replace the classification layer of the backbone with
-        # a new layer that has the desired number of classes
-        self.backbone.fc = torch.nn.Linear(
-            in_features=self.backbone.fc.in_features, out_features=num_classes
-        )
+        # Replace the last fully connected layer with a new one
+        self.model.fc = torch.nn.Linear(self.model.fc.in_features, num_classes)
 
     def forward(self, images):
-        # Use the backbone to extract features from the input images
-        features = self.backbone(images)
-
         # Perform classification on the extracted features
-        return self.backbone.fc(features)
+        return self.model(images)
 
 
 if __name__ == "__main__":
     # Create an instance of the model with the desired number of classes
     model = LogoDetectionModel(num_classes=10)
-    print(model)
     print(summary(model, (3, 256, 256)))
